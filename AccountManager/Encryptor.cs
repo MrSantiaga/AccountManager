6d64ae2a9c1496fb6e6fe125fc43c449c502b0f9
Alex <zhukaleksazhuk@gmail.com>
2024-03-12 00:07:08 +0300

    Create a class encrypt, create menu

---
+++ AccountManager/Encryptor.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AccountManager
+{
+	internal class Encryptor : IDisposable
+	{
+		private Aes _aes;
+		private byte[] _vector;
+
+		public Encryptor() : this("ho-ho-ho")
+		{
+
+		}
+		public Encryptor(string vector)
+		{
+			_aes = Aes.Create();
+			_vector = GetVector(vector);
+			_aes.IV = _vector;
+		}
+
+		public void Dispose()
+		{
+			_aes.Dispose();
+		}
+
+		public string Encrypt(string password, string login)
+		{
+			byte[] loginBytes = GetLogin(login);
+			_aes.Key = loginBytes;
+			ICryptoTransform encryptor = _aes.CreateEncryptor();
+			using MemoryStream ms = new MemoryStream();
+			using CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+			using (StreamWriter sw = new StreamWriter(cs))
+			{
+				sw.Write(password);
+			}
+			byte[] encryptedPassword = ms.ToArray();
+			return Convert.ToBase64String(encryptedPassword);
+
+		}
+		private byte[] GetLogin(string login)
+		{
+			byte[] loginBytes = Encoding.UTF8.GetBytes(login);
+			using SHA256 sha256 = SHA256.Create();
+			return sha256.ComputeHash(loginBytes);
+		}
+		private byte[] GetVector(string vector)
+		{
+			byte[] keyBytes = Encoding.UTF8.GetBytes(vector);
+			using MD5 mD5 = MD5.Create();
+			return mD5.ComputeHash(keyBytes);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
--- AccountManager/Program.cs
+++ AccountManager/Program.cs
@@ -4,7 +4,72 @@
 	{
 		static void Main(string[] args)
 		{
-			// TODO
+			Menu();
+		}
+
+		static public void Menu()
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to Account Manager");
+			Console.WriteLine("Menu: \n1. - Add new credentails \n2. - List all credentiles \n3. - Find and show credentiles \n4. - Delete credentiles \n0. - Exit");
+
+			bool tryParse = int.TryParse(Console.ReadLine(), out int choose);
+			while ((!tryParse) || (choose < 0 || choose > 5))
+			{
+				Console.WriteLine("Unknown value. Try again");
+				int.TryParse(Console.ReadLine(), out choose);
+			}
+
+			switch (choose)
+			{
+				case 1:
+					{
+						Dates user = new Dates();
+						Encryptor first = new Encryptor();
+						Console.WriteLine("Add your dates to sign. \nAdd your login");
+						string login = Console.ReadLine();
+						Console.WriteLine("Add your password");
+						string password = Console.ReadLine();
+						Console.WriteLine("Repeat your password, please");
+						string passwordRepeat = Console.ReadLine();
+						if (password == passwordRepeat)
+						{
+							Console.WriteLine("Success");
+							user.Login = login;
+							user.Password = first.Encrypt(password, login);
+							Console.WriteLine($"User login is  {user.Login}  user password is {user.Password}");
+						}
+						else
+						{
+							Console.WriteLine("Password mismatch");
+						}
+						Console.WriteLine("Do you want to return to menu?\n 1. - Yes\n 2. - No");
+						int answerRetutn = int.Parse(Console.ReadLine());
+						if (answerRetutn == 1)
+						{
+							Menu();
+						}
+						break;
+					}
+				case 2:
+					{
+						break;
+					}
+				case 3:
+					{
+						break;
+					}
+				case 4:
+					{
+						break;
+
+					}
+				case 0:
+					{
+						break;
+					}
+
+			}
 		}
 	}
 }
