b6b5bac3842e637c049a9133e0eb30bb4c526b40
Alex <zhukaleksazhuk@gmail.com>
2024-03-12 00:07:08 +0300

    Create a class encrypt, create menu

---
+++ AccountManager/Alllogins.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountManager
+{
+	internal class Alllogins
+	{
+		List<string> userLogins { get; set; }
+		List<string> userPasswords { get; set; }
+	}
+}
---
+++ AccountManager/Dates.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountManager
+{
+	internal class Dates
+	{
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public List<Alllogins> allLogins { get; set; }
+	}
+}
